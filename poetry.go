cc6e9098ccac0ba1a925232a66634a3810585165
mcclayac <anthony.mcclay@me.com>
2018-12-28 15:04:47 -0600

    Changes in Poetry

--- poetry.go
+++ poetry.go
@@ -1,25 +1,58 @@
 package poetry
 
-import "unicode"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
 
 type Line string
 type Stanza []Line
 type Poem []Stanza
 
-func NewPoem() Poem  {
+func NewPoem() Poem {
 	return Poem{}
+}
+
+func LoadPoem(filename string) (Poem, error) {
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	p := Poem{}
+	var s Stanza
+
+	scan := bufio.NewScanner(f)
+	for scan.Scan() {
+		l := scan.Text()
+		if l == "" {
+			p = append(p, s)
+			s = Stanza{}
+			continue
+		}
+		s = append(s, Line(l))
+	}
+	p = append(p, s)
+
+	if scan.Err() != nil {
+		return nil, scan.Err()
+	}
+	return p, nil
 }
 
 func (p Poem) NumStanzas() int {
 	return len(p)
 }
 
-func (s Stanza) NumLines() int   {
+func (s Stanza) NumLines() int {
 	return len(s)
 }
 
 func (p Poem) NumLines() (count int) {
-	for _,s := range p {
+	for _, s := range p {
 		count += s.NumLines()
 	}
 	return
@@ -27,9 +60,9 @@ func (p Poem) NumLines() (count int) {
 
 func (p Poem) Stats() (numVowels, numConstanants, numPuncuations int) {
 
-	numVowels = 0        // don't have to imitialize to 0
-	numConstanants = 0   // it is done by default as returned
-						 // variable default initializers
+	numVowels = 0      // don't have to imitialize to 0
+	numConstanants = 0 // it is done by default as returned
+	// variable default initializers
 	zeds := 0
 	//fmt.Printf("Starting loop\n\n")
 	for _, s := range p {
@@ -56,7 +89,21 @@ func (p Poem) Stats() (numVowels, numConstanants, numPuncuations int) {
 	//fmt.Printf("Ending loop\n\n")
 
 	//return numVowels, numConstanants
-	return     ///  default return variables
+	return ///  default return variables
 }
 
+func (s Stanza) String() string {
+	result := ""
+	for _, l := range s {
+		result += fmt.Sprintf("%s\n", l)
+	}
+	return result
+}
 
+func (p Poem) String() string {
+	result := ""
+	for _, s := range p {
+		result += fmt.Sprintf("%s\n", s)
+	}
+	return result
+}
